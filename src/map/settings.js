e69d1d630ea5d7f5c3e038c56ebada6501e929c4
Dena DeBry <dena@buttonwillowsix.com>
2024-11-05 17:55:00 -0700

    UE-491:adding stuff to make the map

---
+++ src/map/settings.js
@@ -0,0 +1,116 @@
+var map_cfg = {
+    "mapWidth": "100%",
+    "mapHeight": 320,
+    
+    "shadowWidth": 2,
+    "shadowOpacity": 0.2,
+    "shadowColor": "black",
+    "shadowX": 0,
+    "shadowY": 0,
+    
+    "iPhoneLink": true,
+    "isNewWindow": false,
+    
+    "zoomEnable": false,
+    "zoomEnableControls": false,
+    "zoomIgnoreMouseScroll": false,
+    "zoomMax": 8,
+    "zoomStep": 0.8,
+    
+    "borderColor": "#ffffff",
+    "borderColorOver": "#ffffff",
+    "borderOpacity": 0.5,
+    
+    "nameColor": "#ffffff",
+    "nameColorOver": "#ffffff",
+    "nameFontSize": "10px",
+    "nameFontWeight": "bold",
+    
+    "nameStroke": true,
+    "nameStrokeColor": "#000000",
+    "nameStrokeColorOver": "#000000",
+    "nameStrokeWidth": 1.5,
+    "nameStrokeOpacity": 0.5,
+    
+    "overDelay": 300,
+    "nameAutoSize": false,
+    
+    "tooltipOnHighlightIn": false,
+    "freezeTooltipOnClick": false,
+    
+    "mapId": "Z91FJz",
+    
+    "map_data": {
+    "st1": {
+        "id": 1,
+        "name": "Africa",
+        "shortname": "AFRICA",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    },
+    "st2": {
+        "id": 2,
+        "name": "Asia",
+        "shortname": "ASIA",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    },
+    "st3": {
+        "id": 3,
+        "name": "Australia & Oceania",
+        "shortname": "AUSTRALIA & OCEANIA",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    },
+    "st4": {
+        "id": 4,
+        "name": "Central America",
+        "shortname": "CENTRAL AMERICA",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    },
+    "st5": {
+        "id": 5,
+        "name": "Europe",
+        "shortname": "EUROPE",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    },
+    "st6": {
+        "id": 6,
+        "name": "North America",
+        "shortname": "NORTH AMERICA",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    },
+    "st7": {
+        "id": 7,
+        "name": "South America",
+        "shortname": "SOUTH AMERICA",
+        "link": "",
+        "comment": "",
+        "image": "",
+        "color_map": "#7798bb",
+        "color_map_over": "#366CA3"
+    }
+}
+}
+;
