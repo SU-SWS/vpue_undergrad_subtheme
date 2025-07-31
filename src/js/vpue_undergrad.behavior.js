37a3d3c959e9f55a93e47f51617bfa1c50eced4a
Jen Breese <jbreese@stanford.edu>
2025-07-31 12:46:21 -0700

    UE-546: updates to the subtheme for Utility navigation (#78)
    * UE-546: updates to the subtheme for Utility naviagation
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup for utility and a11y
    
    * fixup to color order
    
    * fixup

---
+++ src/js/vpue_undergrad.behavior.js
@@ -0,0 +1,16 @@
+(function ($, once) {
+  'use strict';
+  Drupal.behaviors.ppoIrsrSubtheme = {
+    attach: function (context) {
+
+    // Move the utility button to the brand bar for mobile users
+      const $utiltyBtn = $('.su-site-header-button', context);
+      if ($utiltyBtn.length) {
+        const $clonedutiltyBtn = $utiltyBtn.clone();
+        $clonedutiltyBtn.prependTo('.su-masthead', context)
+          .wrap('<div class="su-mobile-utility-button"></div>');
+      }
+    }
+  };
+
+})(jQuery, once);
